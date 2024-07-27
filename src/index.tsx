d0663050c82e2be2f56fe5c176247a295b073c18
Multinite <founders.multinite@gmail.com>
2024-07-27 17:36:35 +1000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { createComponent } from "@multinite_official/multiui";
 import { Button } from "./Button";
 import { ButtonLabel } from "./ButtonLabel";
 
@@ -8,3 +9,11 @@ namespace ButtonComponent {
 }
 
 export { ButtonComponent, Button, ButtonLabel };
+
+createComponent({
+  name: "Button",
+  version: "1.0.0",
+  description: "Hello, World!",
+  components: ["Button", "ButtonLabel"],
+  dependant_components: ["ButtonLabel"],
+});
