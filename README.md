062f2bc7513648f1fb8b180df4bbe26749f3eb4e
Multinite <founders.multinite@gmail.com>
2024-07-27 16:15:19 +1000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Button
+
+A button component for [MultiUI](https://github.com/ping/multiui).
+
+## Usage
+
+```js
+import Button from "@/components/multiui/Button";
+
+<Button>Click me</Button>;
+```
+
+## Props
+
+| Name     | Type      | Default | Description           |
+| -------- | --------- | ------- | --------------------- |
+| children | ReactNode |         | The button's content. |
---
+++ index.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Button as RACButton, type ButtonProps } from "react-aria-components";
+import { cn, cn_seperator } from "@multinite_official/multiui";
+
+function Button(
+  props: ButtonProps & {
+    className?: Parameters<typeof cn>[0];
+    children?: React.ReactNode;
+  }
+) {
+  return (
+    <RACButton
+      slot={`Button`}
+      className={cn(
+        "w-fit h-fit",
+        ...(props.className ? [cn_seperator, props.className] : [])
+      )}
+    >
+      {props.children}
+    </RACButton>
+  );
+}
+
+export default Button;
